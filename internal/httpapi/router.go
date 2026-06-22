898901844426201f1eb736bf16d8418c4583d89b
y.krenta <krentawork@gmail.com>
2026-06-22 23:37:43 +0300

    add middleware NewRouter in router.go

--- internal/httpapi/router.go
+++ internal/httpapi/router.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter() http.Handler {
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Logger, middleware.Recoverer)
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("service is ok\n"))
 		if err != nil {
 			return
 		}
 	})
+
 	return r
 }
