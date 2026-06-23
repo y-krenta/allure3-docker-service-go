f906df9c260e07ebb0f62f414079f43f4e6bce71
y.krenta <krentawork@gmail.com>
2026-06-23 11:28:34 +0300

    Add CreateDir to projects package

--- internal/projects/projects.go
+++ internal/projects/projects.go
@@ -2,13 +2,17 @@ package projects
 
 import (
 	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
 	"path/filepath"
 	"regexp"
 )
 
-// `re` is a regular expression that matches strings containing only lowercase letters, digits, underscores, and hyphens. Used for validating project IDs.
 var (
-	re = regexp.MustCompile(`^[a-z0-9_-]+$`)
+	projectIDPattern = regexp.MustCompile(`^[a-z0-9_-]+$`)
+	ErrProjectExists = errors.New("project already exists")
 )
 
 func ResultsDir(baseDir, projectID string) string {
@@ -32,9 +36,44 @@ func ValidateProjectID(id string) error {
 	if len(id) > 200 {
 		return errors.New("project id must not exceed 200 characters")
 	}
-	if !re.MatchString(id) {
+	if !projectIDPattern.MatchString(id) {
 		return errors.New("project id must match ^[a-z0-9_-]+$")
 	}
+
+	return nil
+}
+
+func CreateDir(baseDir, id string) error {
+	err := os.Mkdir(filepath.Join(baseDir, id), 0755)
+
+	if errors.Is(err, fs.ErrExist) {
+		return ErrProjectExists
+	}
+
+	if err != nil {
+		return fmt.Errorf("unable to create project directory: %w", err)
+	}
+
+	ok := false
+	defer func() {
+		if !ok {
+			if rmErr := os.RemoveAll(filepath.Join(baseDir, id)); rmErr != nil {
+				log.Printf("rollback failed: %v\n", rmErr)
+			}
+		}
+	}()
+
+	err = os.MkdirAll(ReportsDir(baseDir, id), 0755)
+	if err != nil {
+		return fmt.Errorf("unable to create reports directory: %w", err)
+	}
+
+	err = os.MkdirAll(ResultsDir(baseDir, id), 0755)
+	if err != nil {
+		return fmt.Errorf("unable to create results directory: %w", err)
+	}
+
+	ok = true
 
 	return nil
 }
